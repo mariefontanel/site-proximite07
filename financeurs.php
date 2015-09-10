9667f6471611845286b4dd85926107acd8497b02
mluquet <mluquet@gmail.com>
2015-09-10 11:06:54 +0200

    ajout page financeurs.php et footer.php, et modif index.php avec balise include pour appeler les pages

---
+++ financeurs.php
@@ -0,0 +1,9 @@
+	<div class="bloc_financeurs">
+				<img src="images/logo_ue_2013.png" class="logo_financeurs">
+				<img src="images/logo_fse.png" class="logo_financeurs">
+				<img src="images/logo_leader.png" class="logo_financeurs">
+				<img src="images/logo_rra_2015.png" class="logo_financeurs">
+				<img src="images/logo_capca.jpg" class="logo_financeurs">
+				<img src="images/logo_cc_lamastre.jpg" class="logo_financeurs">
+				<img src="images/logo_valeyrieux.png" class="logo_financeurs">
+	</div>
