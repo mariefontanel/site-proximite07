99dd96cad348c73eaca2882f082b0a35e1cd9298
mluquet <mluquet@gmail.com>
2015-09-10 21:52:32 +0200

    ajout contenu texte général sur index avec background white

--- index.php
+++ index.php
@@ -19,11 +19,15 @@
 			<div class="bloc_presentation">
 				
 				<!-- <div id="map_canvas"></div> -->
-				 <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2824.2876532463283!2d4.36905!3d44.937819999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f5805cd934bd85%3A0x4291ed20b3f32cab!2sSite+de+Proximit%C3%A9+des+Bouti%C3%A8res!5e0!3m2!1sfr!2sfr!4v1441785993253" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+				 <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2824.2876532463283!2d4.36905!3d44.937819999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f5805cd934bd85%3A0x4291ed20b3f32cab!2sSite+de+Proximit%C3%A9+des+Bouti%C3%A8res!5e0!3m2!1sfr!2sfr!4v1441785993253" width="500" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 				<div class="introduction">
-					<p class="texte_presentation">Le Site de proximité est un pôle d’ingénierie locale pour la création et le développement des activités et des emplois. Il s’adresse, aux particuliers, aux entreprises et aux associations de son périmètre d’action et à ceux qui veulent venir s’y installer. Il offre également des services aux collectivités. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed tempus eros. Proin a fermentum sem. Ut nulla quam, mattis a pellentesque eget, finibus nec turpis. Nunc posuere enim quis dolor consequat, in blandit enim mattis. Proin non dolor commodo, tristique justo a, convallis justo. Praesent interdum, nulla at.</p>
-
+					<p>Le Site de proximité est un pôle d’ingénierie locale pour la création et le développement des activités et des emplois. Il s’adresse, aux particuliers, aux entreprises et aux associations de son périmètre d’action et à ceux qui veulent venir s’y installer.</p>
+					<p> Il offre également ses services aux collectivités locales.</p>
+					<p>Le Site de proximité des Boutières est une association loi 1901, financée par l’Europe, l’Etat, la Région et les intercommunalités. Ses services sont gratuits.</p>
+					<p>Il intervient sur les Communautés de communes du pays de Lamastre, Valeyrieux et sur une partie de la Communauté d’agglomération centre Ardèche.
+					<p>Il travaille en complémentarité avec les services de l’emploi et de la création d’activités. Il est votre porte d’entrée sur le territoire.</p>
+					<p>Son <a href="equipe.php"><strong>équipe</strong></a> vous accueille dans 5 lieux différents : Saint Agrève, Saint Martin de Valamas, Le Cheylard, Lamastre et les Ollières sur Eyrieux.</p>
 				</div>
 				
 			</div>
@@ -32,7 +36,6 @@
 				<div class="bloc_gauche" id="bloc_particuliers">
 					<h2>Particuliers</h2>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed tempus eros. Proin a fermentum sem. Ut nulla quam, mattis a pellentesque eget, finibus nec turpis. Nunc posuere enim quis dolor consequat, in blandit enim mattis. Proin non dolor commodo, tristique justo a, convallis justo. Praesent interdum, nulla at ullamcorper bibendum, massa mauris placerat lacus, ut lacinia ante massa auctor metus. Aenean vitae turpis eu tortor sollicitudin dignissim. Pellentesque mollis nunc sit amet massa pharetra dictum. Nam venenatis, arcu et tempus semper, velit augue iaculis tortor, fringilla imperdiet augue nisi a metus. Fusce erat augue, consequat eu euismod quis, tempor at velit. Quisque tristique magna ac leo hendrerit vehicula. Suspendisse vestibulum purus a erat venenatis, sagittis iaculis velit efficitur.</p>
-					<p>Curabitur ut egestas erat. Nam dictum turpis sed velit pulvinar posuere. Donec sit amet nulla ac risus semper porta. Mauris pellentesque id mi ut pretium. Nunc risus dolor, ullamcorper eget sagittis porttitor, aliquam a enim. Sed elementum risus quis tortor lacinia ultrices. Suspendisse et arcu ac turpis lacinia rhoncus. Aliquam vulputate pharetra velit ac pulvinar. Sed in nulla auctor, placerat enim malesuada, aliquam nibh. Quisque rutrum dolor quis faucibus laoreet. Phasellus nisi enim, eleifend tempor tincidunt at, congue et erat. Morbi facilisis vel sem id ornare. Morbi elit nunc, viverra id dolor non, rutrum laoreet nibh. Vivamus eget lacus quis erat varius maximus.</p>
 				</div>
 				<img src="images/reunion.jpg" class="reunion">
 			</div>
