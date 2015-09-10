b83e739abfbbb184f2ed615cab44bf596c5ac07c
mluquet <mluquet@gmail.com>
2015-09-10 10:46:53 +0200

    ajout header.php et balise include dans index.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<header>
+		<div class="bloc_logo">
+			<img src="images/logo.jpg" class="logo_site">
+		</div>
+		<nav class="bloc_nav">
+		<ul class="menu">
+			<a href="presentation"><li>Qui sommes-nous ?</li></a>
+			<a href="equipe"><li>Notre équipe</li></a>
+			<a href="lieux_accueil"><li>Nos lieux d'accueil</li></a>
+			<a href="form_contact.html"><li>Nous contactez</li></a>
+		</ul>
+		</nav>
+		<div class="bloc_accroche">
+			<h1>Votre outil pour l'emploi et la création d'activités</h1>
+		</div>
+</header>
