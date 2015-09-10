51d684010960b5f62f77e8ad3960583eb612615d
mluquet <mluquet@gmail.com>
2015-09-10 10:51:25 +0200

    modif lien dans header vers pages en php

--- form_contact.html
+++
@@ -1,1 +0,0 @@
-Formulaire de contact
--- header.php
+++ header.php
@@ -4,10 +4,10 @@
 		</div>
 		<nav class="bloc_nav">
 		<ul class="menu">
-			<a href="presentation"><li>Qui sommes-nous ?</li></a>
-			<a href="equipe"><li>Notre équipe</li></a>
-			<a href="lieux_accueil"><li>Nos lieux d'accueil</li></a>
-			<a href="form_contact.html"><li>Nous contactez</li></a>
+			<a href="presentation.php"><li>Qui sommes-nous ?</li></a>
+			<a href="equipe.php"><li>Notre équipe</li></a>
+			<a href="lieux_accueil.php"><li>Nos lieux d'accueil</li></a>
+			<a href="form_contact.php"><li>Nous contactez</li></a>
 		</ul>
 		</nav>
 		<div class="bloc_accroche">
