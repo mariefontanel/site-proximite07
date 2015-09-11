9b3c79c743a873751d733e15412e4cf86fa5f031
mluquet <mluquet@gmail.com>
2015-09-11 15:41:45 +0200

    ajout readme, modif page equipe et css

--- equipe.php
+++ equipe.php
@@ -1,6 +1,14 @@
 <?php include("head.php"); ?>
 <?php include("header.php"); ?>
 
-Notre équipe
+<div class="equipe">
+	<h2>Notre équipe</h2>
+	<ul>
+		<li>Jacqueline Dudal, responsable, Construction d’offres d’activités</li>
+		<li>Sandrine Imbert animatrice emploi formation</li>
+		<li>Sylvie Martin animatrice emploi formation, Construction d’offres d’activités.</li>
+	</ul>
+
+</div>
 
 <?php include("footer.php"); ?>
