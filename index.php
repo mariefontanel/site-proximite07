30acf7c5c16b3ca6b0684dd2c96727353403da75
mluquet <mluquet@gmail.com>
2015-09-11 10:36:17 +0200

    ajout contenu page lieux d'accueil

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
 			<div class="bloc_presentation">
 				
 				<!-- <div id="map_canvas"></div> -->
+				
 				 <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2824.2876532463283!2d4.36905!3d44.937819999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f5805cd934bd85%3A0x4291ed20b3f32cab!2sSite+de+Proximit%C3%A9+des+Bouti%C3%A8res!5e0!3m2!1sfr!2sfr!4v1441785993253" width="500" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 				<div class="introduction">
--- lieux_accueil.php
+++ lieux_accueil.php
@@ -1,6 +1,13 @@
 <?php include("head.php"); ?>
 <?php include("header.php"); ?>
 
-Lieux d'accueil
+<h2>Lieux d'accueil</h2>
+	<div class="stagreve">
+	Saint Agrève, centre socio culturel, Place Verdun</div>
+	<div class="stmartin">Saint Martin de Valamas :quartier le Pont (siège)</div>
+	<div class="lamastre">Lamastre : Communauté de communes, 18, rue Ferdinand Herold</div>
+	<div class="cheylard">Le Cheylard : Relais emploi  – quartier de la gare, Pôleyrieux (Atelier des projets),zone d’Aric</div>
+	<div class="ollieres">Les Ollières : antenne de la Communauté d’agglo Privas centre Ardèche, le Tissage</div>
+
 
 <?php include("footer.php"); ?>
