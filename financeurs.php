7d2cace2e7d9c80e1313f107de3e29c2d5ea714a
mluquet <mluquet@gmail.com>
2015-09-11 16:56:43 +0200

    changement taille logo comcom lamastre

--- financeurs.php
+++ financeurs.php
@@ -4,6 +4,6 @@
 				<img src="images/logo_leader.png" class="logo_financeurs">
 				<img src="images/logo_rra_2015.png" class="logo_financeurs">
 				<img src="images/logo_capca.jpg" class="logo_financeurs">
-				<img src="images/logo_cc_lamastre.jpg" class="logo_financeurs">
+				<img src="images/logo_cc_lamastre_petit.jpg" class="logo_financeurs">
 				<img src="images/logo_valeyrieux.png" class="logo_financeurs">
 	</div>
