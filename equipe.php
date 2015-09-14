a1c4e35ee3c4c2e25a9d4d9e664800412196aee1
[sandrineluquet] <[didpierrot@gmail.com]>
2015-09-14 12:08:58 +0200

    mise en place du texte et du css de la page notre équipe

--- equipe.php
+++ equipe.php
@@ -4,9 +4,10 @@
 <div class="equipe">
 	<h2>Notre équipe</h2>
 	<ul>
-		<li>Jacqueline Dudal, responsable, Construction d’offres d’activités</li>
-		<li>Sandrine Imbert animatrice emploi formation</li>
-		<li>Sylvie Martin animatrice emploi formation, Construction d’offres d’activités.</li>
+		<li>Jacqueline Dudal, responsable, chargée de la construction d’offres d’activités.</li>
+		<li>Nelle Bernanose, chargée de l'appui à la création d'activités.</li>
+		<li>Sandrine Imbert animatrice emploi formation et conseillère Mission locale, sur les secteurs de Saint-Martin-De-Valamas, Saint-Agrève, Les Ollières.</li>
+		<li>Sylvie Martin animatrice emploi formation, et conseillère Mission locale, sur les secteurs de Saint-Martin-De-Valamas, Saint-Agrève, Lamastre.</li>
 	</ul>
 
 </div>
