7f5765c3f0ec6ccf94cee1053fc9f2eb1571ade4
[sandrineluquet] <[didpierrot@gmail.com]>
2015-09-14 13:27:40 +0200

    mise en place des textes de la page lieux acceuil et css de cette page

--- lieux_accueil.php
+++ lieux_accueil.php
@@ -3,12 +3,15 @@
 
 <div class="lieux">
 	<h2>Lieux d'accueil</h2>
-		<div class="stagreve">
-		Saint Agrève, centre socio culturel, Place Verdun</div>
-		<div class="stmartin">Saint Martin de Valamas :quartier le Pont (siège)</div>
-		<div class="lamastre">Lamastre : Communauté de communes, 18, rue Ferdinand Herold</div>
-		<div class="cheylard">Le Cheylard : Relais emploi  – quartier de la gare, Pôleyrieux (Atelier des projets),zone d’Aric</div>
-		<div class="ollieres">Les Ollières : antenne de la Communauté d’agglo Privas centre Ardèche, le Tissage</div>
+		<h4>Nous sommes présents sur rendez-vous dans les lieux suivants: </h4>
+		<ul>
+			<li><div class="stagreve">07320 Saint-Agrève : Centre socio culturel, Place Verdun</div></li>
+			<li><div class="lamastre">07270 Lamastre : Communauté de communes, 18 rue Ferdinand Herold </div></li>
+			<li><div class="cheylard">07160 Le Cheylard : Relais emploi, quartier de la Gare</div></li>
+			<li><div class="cheylard">07160 Le Cheylard : Poleyrieux, zone d'Aric</div></li>
+			<li><div class="ollieres">07360 Les Ollières : Antenne de la Communauté d’agglo Privas centre Ardèche, le Tissage</div></li>
+			<li><div class="stmartin">07310 Saint Martin De Valamas : Quartier le Pont(siège)</div></li>
+		</ul>
 </div>
 
 
