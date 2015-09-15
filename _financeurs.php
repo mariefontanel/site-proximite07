8054857c2596e6c06febddf3c1526ca24548ea15
mluquet <mluquet@gmail.com>
2015-09-15 16:28:05 +0200

    changement font et color sur body

--- _financeurs.php
+++ _financeurs.php
@@ -4,6 +4,6 @@
 				<img src="images/logo_leader.png" class="logo_financeurs">
 				<img src="images/logo_rra_2015.png" class="logo_financeurs">
 				<img src="images/logo_capca_petit.jpg" class="logo_financeurs">
-				<img src="images/logo_cc_lamastre_petit2.jpg" class="logo_financeurs">
+				<img src="images/logo_cc_lamastre_petit.jpg" class="logo_financeurs">
 				<img src="images/logo_valeyrieux.png" class="logo_financeurs">
 	</div>
