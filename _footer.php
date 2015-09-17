3473f94bf1797f79c3f6f27104e4afb0b6132291
mluquet <mluquet@gmail.com>
2015-09-17 22:34:05 +0200

    modif footer div nav

--- _footer.php
+++ _footer.php
@@ -1,17 +1,18 @@
 <footer>
 	<div class="nav">
 	 	<ul class="menu">
-	     <a href="presentation.php"><li>Qui sommes-nous ?</li></a>
-	     <a href="equipe.php"><li>Notre équipe</li></a>
-	     <a href="lieux_accueil.php"><li>Nos lieux d'accueil</li></a>
-	     <a href="form_contact.php"><li>Nous contacter</li></a>
-		 <a href="infos_legales.php" class="infos_legales" >Infos légales</a>
-	   </ul>
-	 </div>
+		     <a href="presentation.php"><li>Qui sommes-nous ?</li></a>
+		     <a href="equipe.php"><li>Notre équipe</li></a>
+		     <a href="lieux_accueil.php"><li>Nos lieux d'accueil</li></a>
+		     <a href="form_contact.php"><li>Nous contacter</li></a>
+			 <a href="infos_legales.php" class="infos_legales" >Infos légales</a>
+	    </ul>
+	</div>
 		
 	<div class="coordonnees">
-	<p>Nos coordonnées</p>
-		<p>Quartier Le Pont,07310 St Martin de Valamas Tél.: 04.75.30.50.82 </p>
+		<p>Nos coordonnées</p>
+		<p>Quartier Le Pont,07310 St Martin de Valamas</p>
+		<p>Tél.: 04.75.30.50.82 </p>
 		<p>Email : <a href="mailto:site.proximite.boutieres@orange.fr">site.proximite.boutieres@orange.fr</a></p>
 	</div>
 	
