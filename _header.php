ec8abcb42c9aa4b7b2373413271a6271fcecaeb4
mluquet <mluquet@gmail.com>
2015-09-17 22:16:11 +0200

    modif margin left titre_accroche plus petit et centrage

--- _header.php
+++ _header.php
@@ -3,7 +3,7 @@
 			<a href="index.php"><img src="images/logo_petit.jpg" class="logo_site"></a>
 		</div>
 		<div class="bloc_accroche">
-			<h1>Votre outil pour l'emploi et la création d'activités</h1>
+			<h2>Votre outil pour l'emploi et la création d'activités</h2>
 		</div>
 	  <div class="home">
         <a href="index.php"><i class="fa fa-home fa-2x"></i></a>
