996351e2ebee96521ea7c96728bef07a5c9c3111
[sandrineluquet] <[didpierrot@gmail.com]>
2015-09-17 16:31:15 +0200

    revu texte coordonnées et ajout n°tel dans lieux

--- lieux_accueil.php
+++ lieux_accueil.php
@@ -6,6 +6,7 @@
 		<h3>Lieux d'accueil</h3>
 			<h4>Nous sommes présents sur rendez-vous dans les lieux suivants: </h4>
 			<ul>
+				<li><div class="stmartin">A Saint Martin De Valamas (07310) : Quartier le Pont(siège)</div></li>
 				<li><div class="stagreve">A Saint Agrève (07320): Centre socio culturel, Place Verdun</div></li>
 				<li><div class="lamastre">A Lamastre (07270) : Communauté de communes, 18 rue Ferdinand Herold </div></li>
 				<li><div class="cheylard">A Le Cheylard (07160) sur 2 sites : </div></li>
@@ -14,8 +15,8 @@
 						<li>Poleyrieux, zone d'Aric</li>
 					</ul>
 				<li><div class="ollieres">Aux Ollières(07360) : Antenne de la Communauté d’agglo Privas centre Ardèche, le Tissage</div></li>
-				<li><div class="stmartin">A Saint Martin De Valamas (07310) : Quartier le Pont(siège)</div></li>
 			</ul>
+		<h4>Pour nous joindre, un seul N° 04 75 30 50 82 </h4>
 	</div>
 </div>	
 
