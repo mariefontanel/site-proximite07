cd0f276a00f0f1d95ace3743364482ed3d53317a
mluquet <mluquet@gmail.com>
2015-10-02 16:17:56 +0200

    modif presentation, footer, img post it

--- _footer.php
+++ _footer.php
@@ -8,7 +8,7 @@
 			<a href="infos_legales.php" class="infos_legales" >Infos légales</a>
 		</div>
 		<div class="logos_partenaire">
-			<img class="cddra"src="images/logo_cddra.jpg.svg">
+			<img class="cddra"src="images/logo_smeov.jpeg">
 			<img class="pnr"src="images/logo_pnr.png">
 			<img class="pam"src="images/logo_pays_ardeche_meridionale.jpeg">
 		</div>	 
