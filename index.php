a9feb584fbb1b067364ae9841cf178774bf0f97b
[sandrineluquet] <[didpierrot@gmail.com]>
2015-10-15 10:24:25 +0200

    remplacement de points par points virgules dans les listes

--- index.php
+++ index.php
@@ -38,11 +38,11 @@
 		<h4>Vous avez des questions sur l’emploi, votre projet professionnel, la formation ?</h4>
 		<p>Le Site de proximité vous propose un <strong>accompagnement individualisé</strong> pour :</p>
 		<ul>
-			<li>faire le point sur vos compétences.</li>
-			<li>structurer votre projet professionnel.</li>
-			<li>vous informer sur l’activité locale, les secteurs porteurs. </li>
-			<li>préparer votre projet de formation.</li>
-			<li>vous outiller dans votre recherche d’emploi (CV, lettres de motivation, préparations d’entretien…).</li>
+			<li>faire le point sur vos compétences;</li>
+			<li>structurer votre projet professionnel;</li>
+			<li>vous informer sur l’activité locale, les secteurs porteurs; </li>
+			<li>préparer votre projet de formation;</li>
+			<li>vous outiller dans votre recherche d’emploi (CV, lettres de motivation, préparations d’entretien…);</li>
 			<li>vous mettre en relation avec les offres d’emploi locales.</li>
 		</ul>
 		<p><strong>Jeunes de 16 à 26 ans,</strong> sur le secteur de Val’Eyrieux, le Site de proximité est correspondant de la <a href="http://ccpu.fr/index.php/site-de-proximite"target=_"blank">Mission locale nord Ardèche</a>. A ce titre, il vous accompagne tout particulièrement dans la mise en place de votre projet professionnel.</p>
@@ -69,9 +69,9 @@
 		<h4>Vous êtes intéressé par la création d’une activité, une entreprise, une association ? </h4>
 		<p><strong>L'Atelier des projets est fait pour vous et vous propose :</strong></p>
 		<ul>
-			<li>un accompagnement individualisé pour valider votre idée.</li>
-			<li>des temps collectifs pour enrichir votre idée. </li>
-			<li>des « agendas pour comprendre » pour explorer un secteur d’activités, un statut, repérer des potentiels de création.</li>
+			<li>un accompagnement individualisé pour valider votre idée;</li>
+			<li>des temps collectifs pour enrichir votre idée; </li>
+			<li>des « agendas pour comprendre » pour explorer un secteur d’activités, un statut, repérer des potentiels de création;</li>
 			<li>une orientation vers les partenaires de la création  pour finaliser votre projet.</li>
 		</ul>
 		<p>Le Site de proximité est labellisé porte d’entrée <a href="https://www.jecree-en-rhonealpes.fr/" target="_blank">« Je crée en Rhône-Alpes »</a> dispositif régional d’accompagnement à la création d’entreprises.</p>
@@ -84,7 +84,7 @@
 		<h4>Vous souhaitez développer ou diversifier votre activité ?</h4>
 		<p>Notre équipe vous apporte :</p>
 		<ul>
-			<li>un appui à la validation et la mise en œuvre de votre projet.</li>
+			<li>un appui à la validation et la mise en œuvre de votre projet;</li>
 			<li>une orientation vers les structures spécialisées pour un appui technique, un financement,…</li>
 		</ul>
 		<h4>Vous voulez recruter ?</h4>	
@@ -109,12 +109,12 @@
 					</div>
 					<div class="modal-body">
 						<ul>
-							<li>Les différents types de contrats.</li>
-							<li>Les aides à l’embauche.</li>
-							<li>La définition du poste proposé.</li>
-							<li>La diffusion des offres.</li>
-							<li>Une mise en relation avec des candidats.</li>
-							<li>Une aide à la sélection et au suivi de l’intégration de la personne recrutée.</li>
+							<li>Les différents types de contrats;</li>
+							<li>Les aides à l’embauche;</li>
+							<li>La définition du poste proposé;</li>
+							<li>La diffusion des offres;</li>
+							<li>Une mise en relation avec des candidats;</li>
+							<li>Une aide à la sélection et au suivi de l’intégration de la personne recrutée;</li>
 							<li>Un accompagnement spécifique pour les formalités liées à votre première embauche.</li>
 						</ul>
 					</div>
@@ -138,7 +138,7 @@
 		<h4>Vous souhaitez développer un nouveau projet, optimiser votre organisation ? </h4>
 		<p>Notre équipe vous apporte :</p>
 		<ul>
-			<li>un appui à la validation et la mise en œuvre de vos projets.</li>
+			<li>un appui à la validation et la mise en œuvre de vos projets;</li>
 			<li>une orientation vers les structures spécialisées.</li>
 		</ul>
 		<h4>Vous voulez recruter ?</h4>
@@ -150,7 +150,7 @@
 		<p>Le Site de proximité vous propose un appui méthodologique pour faire évoluer votre organisation.</p> -->
 		<!-- Modal -->
 		<h4>Vous recherchez des locaux ?</h4>
-		<p>Nous mettons à votre disposition la liste des locaux vacants, vous oriente en fonction de vos besoins.</p>	
+		<p>Nous mettons à votre disposition la liste des locaux vacants, et nous vous orientons en fonction de vos besoins.</p>	
 		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 			<div class="modal-dialog" role="document">
 				<div class="modal-content">
@@ -160,12 +160,12 @@
 					</div>
 					<div class="modal-body">
 						<ul>
-							<li>Les différents types de contrats.</li>
-							<li>Les aides à l’embauche.</li>
+							<li>Les différents types de contrats;</li>
+							<li>Les aides à l’embauche;</li>
 							<li>La définition du poste proposé.</li>
-							<li>La diffusion des offres.</li>
-							<li>Une mise en relation avec des candidats.</li>
-							<li>Une aide à la sélection et au suivi de l’intégration de la personne recrutée.</li>
+							<li>La diffusion des offres;</li>
+							<li>Une mise en relation avec des candidats;</li>
+							<li>Une aide à la sélection et au suivi de l’intégration de la personne recrutée;</li>
 							<li>Un accompagnement spécifique pour les formalités liées à votre première embauche.</li>
 						</ul>
 					</div>
@@ -198,9 +198,9 @@
 				</div>
 			</div>
 			<ul>
-				<li>Information sur les différents types de contrats. 
-					<li>Les aides à l’embauche, aide à la définition du poste proposé.</li>
-					<li>La diffusion de l’offre, mise en relation avec des candidats.</li>
+				<li>Information sur les différents types de contrats; 
+					<li>Les aides à l’embauche, aide à la définition du poste proposé;</li>
+					<li>La diffusion de l’offre, mise en relation avec des candidats;</li>
 					<li>L'appui à la sélection, suivi de l’intégration de la personne recrutée.</li>
 				</ul>
 			</div>
@@ -221,13 +221,13 @@
 			<h4> Pôle d’ingénierie locale, le Site de proximité renforce les synergies locales par l’animation des partenariats :</h4>
 				<p>Il anime ainsi le réseau Emploi Formation Insertion dont les objectifs sont de :</p>
 			<ul>
-				<li>permettre une meilleure connaissance des acteurs de l’emploi, de la formation, et de l’insertion, entre eux.</li>
-				<li>apporter une lisibilité de l’intervention de ces acteurs aux usagers et aux employeurs.</li>
+				<li>permettre une meilleure connaissance des acteurs de l’emploi, de la formation, et de l’insertion, entre eux;</li>
+				<li>apporter une lisibilité de l’intervention de ces acteurs aux usagers et aux employeurs;</li>
 			</ul>	
 			<h4>Il propose de nouvelles actions et missions sur sa zone d’intervention et au-delà</h4>	
 			<ul>
-				<li>diagnostics création d’activités</li>
-				<li>appui à la mise en œuvre de la démarche de construction d’offres d’activités</li>
+				<li>diagnostics création d’activités;</li>
+				<li>appui à la mise en œuvre de la démarche de construction d’offres d’activités;</li>
 				<li>animations d’actions, gestion d’équipement…</li>
 			</ul>	
 		</div>
